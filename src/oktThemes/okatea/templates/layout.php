960a115a68bc8614e5383a1521fad4aae648207a
okatea.org <contact@okatea.org>
2013-08-23 14:12:39 +0200

    okatea theme

--- src/oktThemes/okatea/templates/layout.php
+++ src/oktThemes/okatea/templates/layout.php
@@ -7,7 +7,7 @@ $this->extend('main');
 <div id="page">
 	<header>
 		<div id="head">
-			<div class="title"><?php echo html::escapeHTML(util::getSiteTitle()) ?></div>
+			<div class="title"><a href="<?php echo util::escapeAttrHTML($okt->page->getBaseUrl()) ?>"><?php echo html::escapeHTML(util::getSiteTitle()) ?></a></div>
 			<div class="description"><?php echo html::escapeHTML(util::getSiteDescription()) ?></div>
 		</div><!-- #head -->
 
@@ -34,10 +34,12 @@ $this->extend('main');
 
 	<div id="main">
 
-		<?php # début Okatea : titre SEO de la page (h1)
+		<?php # début Okatea : titre SEO de la page (h1) ou titre si pas de titre SEO
 		if ($okt->page->hasTitleSeo()) : ?>
 		<h1><?php echo html::escapeHtml($okt->page->getTitleSeo()); ?></h1>
-		<?php endif; # fin Okatea : titre SEO de la page (h1) ?>
+		<?php elseif ($okt->page->hasTitle()) : ?>
+		<h1><?php echo html::escapeHtml($okt->page->getTitle()); ?></h1>
+		<?php endif; # fin Okatea : titre SEO de la page (h1) ou titre si pas de titre SEO ?>
 
 		<?php # début Okatea : affichage du contenu de la page
 		echo $this->get('content');
