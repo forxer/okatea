a39b6795f3245b3e94e020f42b76494ccb53656d
okatea.org <contact@okatea.org>
2013-09-06 21:05:21 +0200

    missing translation

--- src/oktModules/galleries/locales/en/admin.items.lang.php
+++ src/oktModules/galleries/locales/en/admin.items.lang.php
@@ -8,3 +8,4 @@ $GLOBALS['__l10n']['m_galleries_items_no_item'] = 'There are no items.';
 $GLOBALS['__l10n']['m_galleries_items_order_drag_drop'] = 'You can change the order of items in dragging/dropping.';
 $GLOBALS['__l10n']['m_galleries_items_action_selected_items'] = 'Action on selected items';
 
+$GLOBALS['__l10n']['m_galleries_item_delete_confirm'] = 'Are you sure you want to delete this image? This action is irreversible.';
--- src/oktModules/galleries/locales/fr/admin.items.lang.php
+++ src/oktModules/galleries/locales/fr/admin.items.lang.php
@@ -8,3 +8,4 @@ $GLOBALS['__l10n']['m_galleries_items_no_item'] = 'Il n’y a aucun élément.';
 $GLOBALS['__l10n']['m_galleries_items_order_drag_drop'] = 'Vous pouvez modifier l’ordre des éléments en les glissants/déposant.';
 $GLOBALS['__l10n']['m_galleries_items_action_selected_items'] = 'Action sur les éléments sélectionnés';
 
+$GLOBALS['__l10n']['m_galleries_item_delete_confirm'] = 'Etes-vous sûr de vouloir supprimer cette image ? Cette action est irréversible.';
