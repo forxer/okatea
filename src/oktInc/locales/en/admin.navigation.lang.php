279a60b919358228005ae1f992e761c82ea63a00
okatea.org <contact@okatea.org>
2013-09-10 10:54:31 +0200

    i18n and changelog

--- src/oktInc/locales/en/admin.navigation.lang.php
+++ src/oktInc/locales/en/admin.navigation.lang.php
@@ -1,17 +1,56 @@
 <?php
 
-$GLOBALS['__l10n']['c_a_config_navigation'] = 'Menus de navigation';
+$GLOBALS['__l10n']['c_a_config_navigation'] = 'Navigation menus';
+$GLOBALS['__l10n']['c_a_config_navigation_config'] = 'Configuration';
 
-$GLOBALS['__l10n']['c_a_config_navigation_menu_must_enter_title'] = 'Vous devez saisir un titre.';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_%s_not_exists'] = 'Menu #%s does not exist.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_%s_not_exists'] = 'The item #%s does not exist.';
 
-$GLOBALS['__l10n']['c_a_config_navigation_menu_added'] = 'Le menu a été ajouté.';
-$GLOBALS['__l10n']['c_a_config_navigation_menu_updated'] = 'Le menu a été mis à jour.';
-$GLOBALS['__l10n']['c_a_config_navigation_menu_switched'] = 'La visiblité du menu a été basculée.';
-$GLOBALS['__l10n']['c_a_config_navigation_menu_deleted'] = 'Le menu a été supprimé.';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_delete_confirm'] = 'Are you sure you want to delete this menu? This action is irreversible.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_delete_confirm'] = 'Are you sure you want to delete this item? This action is irreversible.';
 
-$GLOBALS['__l10n']['c_a_config_navigation_add_menu'] = 'Ajouter un menu';
-$GLOBALS['__l10n']['c_a_config_navigation_edit_menu'] = 'Modifier un menu';
-$GLOBALS['__l10n']['c_a_config_navigation_no_menu'] = 'Il n’y a aucun menu.';
-$GLOBALS['__l10n']['c_a_config_navigation_menus_list'] = 'Liste des menus';
-$GLOBALS['__l10n']['c_a_config_navigation_menu_title'] = 'Titre du menu';
+$GLOBALS['__l10n']['c_a_config_navigation_must_enter_title'] = 'You must enter a title.';
+$GLOBALS['__l10n']['c_a_config_navigation_must_enter_title_in_%s'] = 'You must enter a title in %s.';
 
+$GLOBALS['__l10n']['c_a_config_navigation_must_enter_url'] = 'You must enter an URL.';
+$GLOBALS['__l10n']['c_a_config_navigation_must_enter_url_in_%s'] = 'You must enter an URL in %s.';
+
+$GLOBALS['__l10n']['c_a_config_navigation_menu_added'] = 'The menu was added.';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_updated'] = 'The menu was updated.';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_switched'] = 'The visibility of the menu was switched.';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_deleted'] = 'The menu was removed.';
+
+$GLOBALS['__l10n']['c_a_config_navigation_add_menu'] = 'Add a menu';
+$GLOBALS['__l10n']['c_a_config_navigation_edit_menu'] = 'Edit a menu';
+$GLOBALS['__l10n']['c_a_config_navigation_no_menu'] = 'There is no menu.';
+$GLOBALS['__l10n']['c_a_config_navigation_menus_list'] = 'List of menus';
+$GLOBALS['__l10n']['c_a_config_navigation_menu_title'] = 'Menu title';
+
+$GLOBALS['__l10n']['c_a_config_navigation_manage_items'] = 'Manage items';
+$GLOBALS['__l10n']['c_a_config_navigation_manage_items_menu_%s'] = 'Manage menu %s items.';
+
+$GLOBALS['__l10n']['c_a_config_navigation_menu_items'] = 'Menu items';
+$GLOBALS['__l10n']['c_a_config_navigation_items_%s_menu'] = 'Menu %s items';
+
+$GLOBALS['__l10n']['c_a_config_navigation_item_added'] = 'The item was added.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_updated'] = 'The item was updated.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_switched'] = 'The visibility of the item was switched.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_enabled'] = 'The item was enabled.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_disabled'] = 'The item was disabled.';
+$GLOBALS['__l10n']['c_a_config_navigation_item_deleted'] = 'The item was removed.';
+$GLOBALS['__l10n']['c_a_config_navigation_items_neworder'] = 'The order of the elements was updated.';
+
+$GLOBALS['__l10n']['c_a_config_navigation_add_item'] = 'Add an item';
+$GLOBALS['__l10n']['c_a_config_navigation_add_item_to_%s'] = 'Add an item to %s';
+$GLOBALS['__l10n']['c_a_config_navigation_edit_item'] = 'Edit an item';
+$GLOBALS['__l10n']['c_a_config_navigation_edit_item_of_%s'] = 'Edit an item of %s';
+
+$GLOBALS['__l10n']['c_a_config_navigation_no_item'] = 'There are no items in this menu.';
+$GLOBALS['__l10n']['c_a_config_navigation_one_item'] = 'There is an item in the menu:';
+$GLOBALS['__l10n']['c_a_config_navigation_%s_items'] = 'There are %s items in this menu:';
+
+$GLOBALS['__l10n']['c_a_config_navigation_item_title'] = 'Title of the item';
+$GLOBALS['__l10n']['c_a_config_navigation_item_title_in_%s'] = 'Title of the item in %s';
+
+$GLOBALS['__l10n']['c_a_config_navigation_item_url'] = 'URL of the item';
+$GLOBALS['__l10n']['c_a_config_navigation_item_url_in_%s'] = 'URL of the item in %s';
