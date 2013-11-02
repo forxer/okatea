c73ce0c199f6edfb9325369a269df4b46ccf26c0
okatea.org <contact@okatea.org>
2013-11-02 18:38:49 +0100

    okatea theme

--- src/oktThemes/okatea/locales/fr/custom.lang.php
+++ src/oktThemes/okatea/locales/fr/custom.lang.php
@@ -1,4 +1,10 @@
 <?php
 
+$GLOBALS['__l10n']['Download'] = 'Télécharger';
+$GLOBALS['__l10n']['Stable'] = 'Stable';
+$GLOBALS['__l10n']['Dev'] = 'Dev';
+$GLOBALS['__l10n']['View sources on GitHub'] = 'Afficher les sources sur GitHub';
+
 $GLOBALS['__l10n']['Latest news'] = 'Dernières actualités';
+
 $GLOBALS['__l10n']['Proudly propulsed by Okatea'] = 'Fièrement propulsé par Okatea';
