1de18e444b7aed757364d68b3a17816c9f506d4c
okatea.org <contact@okatea.org>
2013-11-02 13:48:18 +0100

    remove this

--- src/oktModules/news/_install/tpl/news/insert/simple-list-with-excerpt/_define.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->aCurrent = array(
-	'name' 		=> 'Simple list with excerpt',
-	'desc' 		=> 'The template for displaying the insert news in a simple list with excerpt.',
-	'version' 	=> '1.0',
-	'author' 		=> 'okatea.org',
-	'tags' 		=> 'news, insert, simple list,  excerpt'
-);
