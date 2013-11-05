7b4873f44ac42f731e2f40b96adb4620fce01d66
okatea.org <contact@okatea.org>
2013-11-05 11:06:16 +0100

    add okatea_dot_org module

---
+++ src/oktModules/okatea_dot_org/_define.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @ingroup okt_module_okatea_dot_org
+ * @brief Fichier de définition du module
+ *
+ */
+
+$this->registerModule(array(
+	'name' 			=> 'okatea.org',
+	'desc' 			=> 'Module for the specific needs of the okatea.org website.',
+	'version' 		=> '1.0',
+	'author' 		=> 'okatea.org'
+));
---
+++ src/oktModules/okatea_dot_org/module_handler.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @ingroup okt_module_okatea_dot_org
+ * @brief La classe principale du Module Okatea.org.
+ *
+ */
+
+class module_okatea_dot_org extends oktModule
+{
+	protected $sUrl;
+	protected $sCacheFile;
+
+	protected $aVersionInfo = array(
+		'version' => null,
+		'href' => null,
+		'checksum' => null,
+		'info' => null
+	);
+
+	protected $sCacheTtl = '-24 hours';
+
+	protected function prepend()
+	{
+		$this->sUrl = 'http://repository.okatea.org/packages/versions.xml';
+	}
+
+	protected function prepend_admin()
+	{
+
+	}
+
+	protected function prepend_public()
+	{
+
+	}
+
+
+	protected function resetVersionInfos()
+	{
+		$this->aVersionInfo = array(
+			'version' => null,
+			'href' => null,
+			'checksum' => null,
+			'info' => null
+		);
+	}
+
+	/**
+	 * Récupération des informations de version sur le dépot distant.
+	 *
+	 */
+	public function getVersionInfo($sVersionType)
+	{
+		$this->resetVersionInfos();
+
+		$sVersionType = ($sVersionType == 'dev'  ? 'dev' : 'stable');
+
+		$this->sCacheFile = OKT_CACHE_PATH.'/releases/okatea-'.$sVersionType;
+
+		# Check cached file
+		if (is_readable($this->sCacheFile) && filemtime($this->sCacheFile) > strtotime($this->sCacheTtl))
+		{
+			$c = @file_get_contents($this->sCacheFile);
+			$c = @unserialize($c);
+
+			if (is_array($c))
+			{
+				$this->aVersionInfo = $c;
+				return $this->aVersionInfo;
+			}
+		}
+
+		$sCacheDir = dirname($this->sCacheFile);
+
+		$bCanWrite = (!is_dir($sCacheDir) && is_writable(dirname($sCacheDir)))
+			|| (!file_exists($this->sCacheFile) && is_writable($sCacheDir))
+			|| is_writable($this->sCacheFile);
+
+		# If we can't write file, don't bug host with queries
+		if (!$bCanWrite) {
+			return $this->aVersionInfo;
+		}
+
+		if (!is_dir($sCacheDir))
+		{
+			try {
+				files::makeDir($sCacheDir);
+			}
+			catch (Exception $e) {
+				return $this->aVersionInfo;
+			}
+		}
+
+		# Try to get latest version number
+		try
+		{
+			$sPath = '';
+			$oClient = netHttp::initClient($this->sUrl, $sPath);
+
+			if ($oClient !== false)
+			{
+				$oClient->setTimeout(4);
+				$oClient->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+				$oClient->get($sPath);
+
+				$this->readVersion($oClient->getContent(), $sVersionType);
+			}
+		}
+		catch (Exception $e) {}
+
+		# Create cache
+		file_put_contents($this->sCacheFile, serialize($this->aVersionInfo));
+
+		return $this->aVersionInfo;
+	}
+
+	protected function readVersion($str, $sVersionType)
+	{
+		try
+		{
+			$xml = new SimpleXMLElement($str, LIBXML_NOERROR);
+			$r = $xml->xpath("/versions/subject[@name='okatea']/release[@name='".$sVersionType."']");
+
+			if (!empty($r) && is_array($r))
+			{
+				$r = $r[0];
+				$this->aVersionInfo['version'] = isset($r['version']) ? (string) $r['version'] : null;
+				$this->aVersionInfo['href'] = isset($r['href']) ? (string) $r['href'] : null;
+				$this->aVersionInfo['checksum'] = isset($r['checksum']) ? (string) $r['checksum'] : null;
+				$this->aVersionInfo['info'] = isset($r['info']) ? (string) $r['info'] : null;
+			}
+		}
+		catch (Exception $e)
+		{
+			throw $e;
+		}
+	}
+
+
+} # class
