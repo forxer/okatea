73d9b84b8d15b4bbcf8babc7b25c37372d48b0a2
okatea.org <contact@okatea.org>
2013-11-12 13:20:43 +0100

    estimate module advancement

--- src/oktModules/estimate/locales/en/admin.lang.php
+++ src/oktModules/estimate/locales/en/admin.lang.php
@@ -20,30 +20,3 @@ $GLOBALS['__l10n']['m_estimate_product_must_enter_title'] = 'You must enter a ti
 $GLOBALS['__l10n']['m_estimate_accessory_%s_not_exists'] = 'The accessory #%s does not exist.';
 $GLOBALS['__l10n']['m_estimate_accessory_must_enter_title'] = 'You must enter a title.';
 $GLOBALS['__l10n']['m_estimate_accessory_must_choose_title'] = 'You must choose a product.';
-
-# config
-$GLOBALS['__l10n']['m_estimate_configuration'] = 'Configuration';
-$GLOBALS['__l10n']['m_estimate_config_tab_general'] = 'General';
-
-$GLOBALS['__l10n']['m_estimate_config_enable_accessories'] = 'Enable accessories management';
-
-$GLOBALS['__l10n']['m_estimate_config_default_items_number'] = 'Default number of items on the form';
-$GLOBALS['__l10n']['m_estimate_config_default_products_number'] = 'Default number of products';
-$GLOBALS['__l10n']['m_estimate_config_default_accessories_number'] = 'Default number of accessories';
-
-$GLOBALS['__l10n']['m_estimate_config_email_notifications'] = 'Email notifications';
-$GLOBALS['__l10n']['m_estimate_config_enable_notifications'] = 'Enable email notifications';
-$GLOBALS['__l10n']['m_estimate_config_notifications_recipients'] = 'Notification address recipients';
-$GLOBALS['__l10n']['m_estimate_config_notifications_recipients_note_1'] = 'If you do not specify address, notifications will be sent to the main recipient address of the website (%s).';
-$GLOBALS['__l10n']['m_estimate_config_notifications_recipients_note_2'] = 'You can specify multiple addresses separeted by comma.';
-
-
-$GLOBALS['__l10n']['m_estimate_config_tab_tpl'] = 'Templates';
-$GLOBALS['__l10n']['m_estimate_config_tab_tpl_title'] = 'Templates display';
-$GLOBALS['__l10n']['m_estimate_config_tpl_form'] = 'Estimate form templates';
-$GLOBALS['__l10n']['m_estimate_config_tpl_summary'] = 'Estimate summary templates';
-
-$GLOBALS['__l10n']['m_estimate_form_url_from_%s_in_%s'] = 'URL of the page the estimate request form from %s in %s';
-$GLOBALS['__l10n']['m_estimate_summary_url_from_%s_in_%s'] = 'URL de la page de récapitulatif de demande de devis depuis %s en %s';
-
-
