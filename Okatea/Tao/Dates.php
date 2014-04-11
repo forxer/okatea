63746fda97144a06e4dba666847870b9a0966910
Vincent Garnier <forxer@gmail.com>
2014-04-11 10:55:07 +0200

    Revert "Revert "dates management""
    This reverts commit 3a09501ac5a9973b22dfe1bb037b62aa659edb79.

--- Okatea/Tao/Dates.php
+++ Okatea/Tao/Dates.php
@@ -8,8 +8,10 @@
 
 namespace Okatea\Tao;
 
+use Carbon\Carbon;
 use DateTime;
 use IntlDateFormatter;
+use Okatea\Tao\Misc\Utilities;
 
 class Dates
 {
@@ -17,40 +19,99 @@ class Dates
 
 	protected static $sTimezone = 'UTC';
 
+	/**
+	 * Define locale to use.
+	 * @param string $sLocale
+	 */
 	public static function setLocale($sLocale)
 	{
 		self::$sLocale = $sLocale;
 	}
 
+	/**
+	 * Define timezone to use.
+	 * @param string $sTimezone
+	 */
 	public static function setTimezone($sTimezone)
 	{
 		self::$sTimezone = $sTimezone;
 	}
 
-	public static function full(DateTime $date, $bWithTime = false)
+	/**
+	 * Format the date/time value as a string in completely specified style (Tuesday, April 12, 1952 AD or 3:30:42pm PST)
+	 *
+	 * @param mixed $mDate
+	 * @param boolean $bWithTime
+	 * @return string
+	 */
+	public static function full($mDate = null, $bWithTime = false)
 	{
-		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::FULL, self::getTimeType($bWithTime)))
-			->format($date);
+		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::FULL, self::getTimeType($bWithTime), self::$sTimezone))
+			->format(self::getDate($mDate));
 	}
 
-	public static function long(DateTime $date, $bWithTime = false)
+	/**
+	 *
+	 * @param mixed $mDate
+	 * @param boolean $bWithTime
+	 * @return string
+	 */
+	public static function long($mDate = null, $bWithTime = false)
 	{
-		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::LONG, self::getTimeType($bWithTime)))
-			->format($date);
+		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::LONG, self::getTimeType($bWithTime), self::$sTimezone))
+			->format(self::getDate($mDate));
 	}
 
-	public static function medium(DateTime $date, $bWithTime = false)
+	/**
+	 *
+	 * @param mixed $mDate
+	 * @param boolean $bWithTime
+	 * @return string
+	 */
+	public static function medium($mDate = null, $bWithTime = false)
 	{
-		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::MEDIUM, self::getTimeType($bWithTime)))
-			->format($date);
+		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::MEDIUM, self::getTimeType($bWithTime), self::$sTimezone))
+			->format(self::getDate($mDate));
 	}
 
-	public static function short(DateTime $date, $bWithTime = false)
+	/**
+	 *
+	 * @param mixed $mDate
+	 * @param boolean $bWithTime
+	 * @return string
+	 */
+	public static function short($mDate = null, $bWithTime = false)
 	{
-		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::SHORT, self::getTimeType($bWithTime)))
-			->format($date);
+		return (new IntlDateFormatter(self::$sLocale, IntlDateFormatter::SHORT, self::getTimeType($bWithTime), self::$sTimezone))
+			->format(self::getDate($mDate));
 	}
 
+	/**
+	 *
+	 * @param unknown $mDate
+	 * @return Ambigous <\Carbon\Carbon, \Carbon\Carbon>|\DateTime|\Carbon\Carbon
+	 */
+	protected static function getDate($mDate)
+	{
+		if (null === $mDate) {
+			return Carbon::now(self::$sTimezone);
+		}
+		elseif ($mDate instanceof DateTime) {
+			return $mDate;
+		}
+		elseif (Utilities::isInt($mDate)) {
+			return Carbon::createFromTimestamp($mDate, self::$sTimezone);
+		}
+		else {
+			return Carbon::parse($mDate, self::$sTimezone);
+		}
+	}
+
+	/**
+	 *
+	 * @param boolean $bWithTime
+	 * @return number|string
+	 */
 	protected static function getTimeType($bWithTime = false)
 	{
 		if ($bWithTime) {
