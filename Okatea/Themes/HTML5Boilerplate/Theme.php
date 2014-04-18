70c38bd242f33fadc4248dcda2baf29e026dff68
Vincent Garnier <forxer@gmail.com>
2014-04-18 10:31:01 +0200

    stuff in themes

---
+++ Okatea/Themes/HTML5Boilerplate/Theme.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Okatea.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Okatea\Themes\HTML5Boilerplate;
+
+use Okatea\Tao\Extensions\Themes\Theme as baseTheme;
+
+class Theme extends baseTheme
+{
+	public function prepend_public()
+	{
+
+	}
+}
---
+++ Okatea/Themes/HTML5Boilerplate/_define.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Okatea.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+	'name' 			=> "HTML 5 Boilerplate",
+	'desc' 			=> 'Basic theme to make your own.',
+	'version' 		=> '1.0',
+	'author' 		=> 'okatea.org',
+	'tags' 			=> ''
+);
