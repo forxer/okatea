80bc87fd56444c6a0f643b8259f0f88906dbcaa4
Vincent Garnier <forxer@gmail.com>
2014-04-18 12:58:50 +0200

    renaming the old modules, do not delete them for convenience for the moment

--- Okatea/Modules/contact-old/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @brief Le module Contact.
- *
- * @ingroup okt_modules
- * @addtogroup okt_module_contact Contact
- */
--- Okatea/Modules/contact-old/install/templates/map/default/_define.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->aCurrent = array(
-	'name' 			=> 'Default',
-	'desc' 			=> 'The default template for displaying access map page.',
-	'version' 		=> '1.0',
-	'author' 		=> 'okatea.org',
-	'tags' 			=> 'default, access map'
-);
