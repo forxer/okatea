606090e456218852fa9c0b2ec998fbb9f7c0b7eb
Vincent Garnier <forxer@gmail.com>
2014-06-13 09:07:06 +0200

    start new media manager

---
+++ Okatea/Modules/MediaManager/_define.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of Okatea.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return array(
+	'name' => 'Media Manager',
+	'desc' => 'Allow to add a media manager system.',
+	'version' => '2.0-rc1',
+	'author' => 'okatea.org'
+);
